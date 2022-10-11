9931ccbefda0b5aa49d51f8079c25522de7b2c8b
Reena kumari <reenakvs1@gmail.com>
2022-10-11 11:32:51 +0530

    conditinal statements DAY @ problems added

---
+++ DAY 2/prob4.js
@@ -0,0 +1,18 @@
+// Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+
+let user="reena";
+let pass="444";
+
+let user_id="reena";
+let pass_set="444";
+
+if(user==user_id){
+   if(pass==pass_set){
+     console.log("Login Successful")
+   }else{
+     console.log("Incorrect password")
+   }
+}else{
+  console.log("Wrong Credentials")
+}
+
