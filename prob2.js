293a2eb50c94f33e4f77d8f76c6039f3a4bfb145
Reena kumari <reenakvs1@gmail.com>
2022-10-11 11:02:06 +0530

    variables Day 1 problems added

---
+++ prob2.js
@@ -0,0 +1,18 @@
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+
+let name="Reena"
+console.log(name);
+
+
+name="Suresh";
+console.log(name);
+
+name="Mamta";
+console.log(name);
---
+++ prob4.js
@@ -0,0 +1,16 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+console.log("*****************_REPORT CARD_******************");
+console.log("*  NAME:             AKASH                     *");
+console.log("*  SCHOOL:           AKASH NATIONAL SCHOOL     *");
+console.log("*  GRADE:            5th                       *");
+console.log("*  SECTION:          A                         *");
+console.log("*  ROLL NO:          18                        *");
+
+console.log("------------------------------------------------");
+console.log("*  SUBJECTS           MARKS OBT.               *");
+console.log("*  MATHS:             90                       *");
+console.log("*  PHYSICS:           99                       *"); 
+console.log("*  CHEMISTRY:         95                       *"); 
+console.log("************************************************");
