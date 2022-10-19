8df0b130908d5b536f7b26aa0f73bc6619e99f2d
Reena kumari <reenakvs1@gmail.com>
2022-10-19 11:58:12 +0530

    Day * objects1 problems added

---
+++ DAY 8 Objects-I repeat/prob1.js
@@ -0,0 +1,64 @@
+// Given an array find the unique items in the array
+
+
+let arr=["Anuj","Kehlani","Wahran","Dubai","Hakikat","Dubai","Chilled","Chilled"]
+let obj={};
+
+for(let i=0;i<arr.length;i++){
+  let key=arr[i];
+  obj[key]=1;
+  }
+console.log(obj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DAY 8 Objects-I repeat/prob2.js
@@ -0,0 +1,14 @@
+// Given a string print, the number of times each character appears
+
+
+let str="school";
+let obj={}
+
+for(let i=0;i<str.length;i++){
+    if(obj[str[i]]==undefined){
+       obj[str[i]]=1;
+    }else{
+     obj[str[i]]++
+  }
+}
+ console.log(obj);
